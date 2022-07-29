a8eff977e4d93f442811040d6bae531057b1df33
Franso <francismuasya@gmail.com>
2022-07-29 09:04:11 +0300

    create food and user routes

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "github.com/franso/restaurant-management/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.GET("/users", controller.GetUsers())
+	incomingRoutes.GET("/user/:user_id", controller.GetUser())
+	incomingRoutes.POST("/users/signup", controller.SignUp())
+	incomingRoutes.POST("/users/login", controller.Login())
+}
