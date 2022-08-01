87f4359144cb398351378ea3bfdfbac7f2a5868b
Franso <francismuasya@gmail.com>
2022-08-01 16:23:34 +0300

    add food model

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,13 +1,26 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetFoods() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+
+	}
 }
 
 func GetFood() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Params.ByName("food_id")
+
+		defer cancel()
+
+	}
 }
 
 func CreateFood() gin.HandlerFunc {
