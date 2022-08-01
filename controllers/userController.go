ef6995ada715dcf1d49b01975a17d70086294974
Franso <francismuasya@gmail.com>
2022-08-01 11:05:03 +0300

    create user controller function outline and signatures

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,3 +1,25 @@
 package controllers
 
-func GetUser() {}
+import "github.com/gin-gonic/gin"
+
+func GetUsers() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func Login() gin.HandlerFunc {
+	return func(c *gin.Context) {}
+}
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, providedPassword string) bool {}
