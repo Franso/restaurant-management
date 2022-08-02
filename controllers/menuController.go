adc8042f9d6053214f52a4108c3518ef11c3eccf
Franso <francismuasya@gmail.com>
2022-08-02 14:59:43 +0300

    implement the GetFood controller function

--- controllers/menuController.go
+++ controllers/menuController.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/franso/restaurant-management/database"
+	"github.com/franso/restaurant-management/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -32,11 +33,27 @@ func GetMenus() gin.HandlerFunc {
 }
 
 func GetMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		menuId := c.Param("menu_id")
+		var menu models.Menu
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": menuId}).Decode(&menu)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the menu item"})
+		}
+
+		c.JSON(http.StatusOK, menu)
+
+	}
 }
 
 func CreateMenu() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+
+	}
 }
 
 func UpdateMenu() gin.HandlerFunc {
