2d1aa8435d73ddb155bf444bfc8c7846cb3ae383
Franso <francismuasya@gmail.com>
2022-08-02 12:29:05 +0300

    implement GetFood function

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -2,10 +2,19 @@ package controllers
 
 import (
 	"context"
+	"net/http"
 	"time"
 
+	"github.com/franso/restaurant-management/database"
+	"github.com/franso/restaurant-management/models"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	// "gopkg.in/mgo.v2/bson"
+	"go.mongodb.org/mongo-driver/bson"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -17,8 +26,17 @@ func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
 		foodId := c.Params.ByName("food_id")
+		var food models.Food
 
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
 		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+
+		}
+
+		c.JSON(http.StatusOK, food)
 
 	}
 }
