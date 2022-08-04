0dddd68910f94f6e7bd965f57c030bd2c5269965
Franso <francismuasya@gmail.com>
2022-08-04 11:23:48 +0300

    add the invoiceViewFormat struct

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,9 +1,30 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/franso/restaurant-management/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type InvoiceViewFormat struct {
+	Invoice_id       string
+	Payment_method   string
+	Order_id         string
+	Payment_status   *string
+	Payment_due      interface{}
+	Table_number     interface{}
+	Payment_due_date time.Time
+	Order_details    interface{}
+}
+
+var invoiceCollection *mongo.Collection = database.OpenCollection(database.Client, "invoice")
 
 func GetInvoices() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+
+	}
 }
 
 func GetInvoice() gin.HandlerFunc {
