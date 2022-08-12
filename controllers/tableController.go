3c7c33b3fbe34000637ca65dde41d706caa994b9
Franso <francismuasya@gmail.com>
2022-08-12 12:22:48 +0300

    Add - Get Tables controller function

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -1,15 +1,36 @@
 package controllers
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/franso/restaurant-management/database"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "table")
 
 func GetTables() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		result, err := tableCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while listing the tables"})
+
+		}
+		var allTables []bson.M
+
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allTables)
+	}
 }
 
 func GetTable() gin.HandlerFunc {
