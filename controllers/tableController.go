801733c0a96073a0bc1e9e6a8173f8f53fa96ac6
Franso <francismuasya@gmail.com>
2022-08-12 12:29:32 +0300

    Add - Get Table controller function

--- controllers/tableController.go
+++ controllers/tableController.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/franso/restaurant-management/database"
+	"github.com/franso/restaurant-management/models"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -34,7 +35,19 @@ func GetTables() gin.HandlerFunc {
 }
 
 func GetTable() gin.HandlerFunc {
-	return func(c *gin.Context) {}
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		tableId := c.Param("table_id")
+		var table models.Table
+
+		err := tableCollection.FindOne(ctx, bson.M{"table_id": tableId}).Decode(&table)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error while getting table item"})
+			return
+		}
+		c.JSON(http.StatusOK, table)
+	}
 }
 
 func CreateTable() gin.HandlerFunc {
