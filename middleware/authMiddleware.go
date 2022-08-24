6fd92afbc2cf826ca9c226e96bc60ae444e76a70
Franso <francismuasya@gmail.com>
2022-08-24 12:23:52 +0300

    Add - authentication middleware function

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,9 +1,26 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	helper "github.com/franso/restaurant-management/helpers"
+	"github.com/gin-gonic/gin"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
+		clientToken := c.Request.Header.Get("token")
+
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("no authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		}
 	}
 }
